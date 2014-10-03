02a55700837d66a27c487044445063a044b60d8d
Andy Chambers <anchambers@paypal.com>
2014-10-02 23:26:09 -0400

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,16 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.9"]]
+
+                 [com.datomic/datomic-free "0.9.4899"]
+                 [cddr/integrity "0.3.0-SNAPSHOT"]
+                 [ring/ring-json "0.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [compojure "1.1.9"]
+
+                 [prismatic/schema "0.2.6"]
+                 [clj-time "0.8.0"]
+                 [cheshire "5.3.1"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler crud.handler/app}
   :profiles
