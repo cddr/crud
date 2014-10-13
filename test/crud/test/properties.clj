efb26f70b377d1ad0f8505c74055a4e500945d0f
Andy Chambers <anchambers@paypal.com>
2014-10-13 11:55:30 -0400

    Remove unused files

--- test/crud/test/properties.clj
+++
@@ -1,7 +0,0 @@
-(ns crud.test.properties
-  (:require [crud.resource :as r]
-            [clojure.test.check :as tc]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]))
-
-
